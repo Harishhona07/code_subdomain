c2df5459b14e630876de50c18f5deebefe958436
Harish Hona <95570408+Harishhona07@users.noreply.github.com>
2024-01-12 14:21:48 +0545

    Add files via upload

---
+++ project/contact-form1/script.js
@@ -0,0 +1,34 @@
+function showPlaceholder1() {
+    document.getElementById("placeholder1").style.display="block";
+    document.getElementById("name").style.height="40px";
+}
+     function hidePlaceholder1() {
+     document.getElementById("placeholder1").style.display = "none";
+     document.getElementById("name").style.height = "30px";
+     }
+function showPlaceholder2() {
+    document.getElementById("placeholder2").style.display="block";
+    document.getElementById("email").style.height="40px";
+}
+     function hidePlaceholder2() {
+      document.getElementById("placeholder2").style.display = "none";
+      document.getElementById("email").style.height = "30px";
+     }
+function showPlaceholder3() {
+    document.getElementById("placeholder3").style.display="block";
+    document.getElementById("subject").style.height="40px";
+}
+     function hidePlaceholder3() {
+      document.getElementById("placeholder3").style.display = "none";
+      document.getElementById("subject").style.height = "30px";
+     }
+function showPlaceholder4() {
+    document.getElementById("placeholder4").style.display="block";
+    // document.getElementById("message").style.height="40px";
+}
+function hidePlaceholder4() {
+    document.getElementById("placeholder4").style.display = "none";
+
+   }
+
+
